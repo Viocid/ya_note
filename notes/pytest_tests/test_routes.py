db35dec56032af3da0f55b0d68a361cc7c93f56b
Konstantin <153995261+Viocid@users.noreply.github.com>
2024-06-10 14:11:30 +0000

    Repli_pytest_start

---
+++ notes/pytest_tests/test_routes.py
@@ -0,0 +1,15 @@
+# test_routes.py
+import pytest 
+from http import HTTPStatus
+
+from django.urls import reverse
+
+
+@pytest.mark.parametrize(
+    'name',
+    ('notes:home', 'users:login', 'users:logout', 'users:signup')
+)
+def test_pages_availability_for_anonymous_user(client, name):
+    url = reverse(name)
+    response = client.get(url)
+    assert response.status_code == HTTPStatus.OK
