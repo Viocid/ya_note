ef2cddbb0106ef4b32dc28994b25170fe50ec355
Konstantin <153995261+Viocid@users.noreply.github.com>
2024-06-14 09:55:42 +0000

    Repli_Rev_V2_3

--- notes/tests/base_class_data.py
+++ notes/tests/base_class_data.py
@@ -1,8 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
-
-from notes.forms import NoteForm
 from notes.models import Note
 
 User = get_user_model()
--- notes/tests/test_content.py
+++ notes/tests/test_content.py
@@ -1,7 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
-
 from notes.forms import NoteForm
 from notes.tests.base_class_data import BaseClassData
 
